982bfa5b7908c859b5959aef56ca65d5f93c2811
yuto <y.yatsumimi@gmail.com>
2023-09-13 19:58:00 +0900

    feat:python ping ,but cannot ping. this commit is just a report

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 import eel
 import subprocess
+import time
 
 eel.init("web")
 
 @eel.expose
 def ping_host(host):
+  while True:
     try:
         res = subprocess.run(["ping", host, "-c", "1", "-W", "3000"],
                          stdout=subprocess.PIPE,text=True)
-        return res.stdout
+        response = res.stdout
+        print(response)
     except subprocess.CalledProcessError as e:
-        return str(e)
+        print("エラー:", str(e))
+    
+    time.sleep(1)
     
 if __name__ == "__main__":
     eel.start("index.html", size=(1024, 768), port=8083)
--- web/ping.js
+++ web/ping.js
@@ -7,28 +7,21 @@ const pingResultValue = document.getElementById("ping-value")
 const viewerModeText = document.getElementById("network-message")
 const onlineStatusIndicator = document.getElementById("check-online")
 
-let pingRunning = false;//最初はpingを走らせない
-let pingResultElement;
 const host = ipAddressInput.value;//pingが通信する相手は、入力されたIPアドレス
 
 connectIpAddressBtn.addEventListener('click', async () => {
-  //python疎通確認
-  pingRunning = true;
-  const startTime = new Date().getTime(); // ping開始時間
-  const result = await eel.ping_host(host)(); // ping_host関数を非同期で呼び出す
-  const endTime = new Date().getTime(); // ping終了時間
-  const pingTime = endTime - startTime; // ping実行時間
-  if (!pingResultElement) {
-    pingResultElement = document.createElement("span");
-    pingResultElement.classList.add("new-span-class");
-    pingResultValue.parentNode.appendChild(pingResultElement);
-  }
-  //pingの値を上書き(現在では、IPアドレスが読み込めなくてもオンラインと返してしまう)
-  pingResultElement.textContent = `${pingTime}ミリ秒 )`;
-  viewerModeText.innerText = "オンライン "
-  onlineStatusIndicator.style.backgroundColor = "lightgreen"
+  try {
+    const res = await eel.ping_host(host);
 
-  await new Promise(resolve => setTimeout(resolve, 1000));
+    pingResultValue.textContent = `Ping Result: ${res}`
+    viewerModeText.innerText = "オンライン "
+    onlineStatusIndicator.style.backgroundColor = "lightgreen"
+  } catch (error) {
+    pingResultValue.textContent = "通信データなし"
+    viewerModeText.innerText = "オフライン "
+    onlineStatusIndicator.style.backgroundColor = "lightgray"
+  }
+  await new Promise(resolve => setTimeout(resolve, 1000))
 
   if (connectIpAddressBtn.innerText === "編集") {
     connectIpAddressBtn.innerText = "保存";
@@ -45,51 +38,19 @@ connectIpAddressBtn.addEventListener('click', async () => {
 });
 
 //リロード時に、IPアドレスの値が入力されていたら、カメラビューワーに反映させる。
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
   // localStorageからIPアドレスを取得
   const storedIpAddress = localStorage.getItem('ip-address');
   if (storedIpAddress) {
     // IPアドレスの入力フィールドに値を設定
     ipAddressInput.value = storedIpAddress;
-
     // カメラビューワーに反映
     cameraViewer.src = "http://" + storedIpAddress + "/ImageViewer?Mode=Motion&Resolution=640x360&Quality=Standard&Interval=10";
-
-    // Pingを1秒おきに実行
-    setInterval(() => {
-      pingHost(storedIpAddress);
-    }, 1000);
   }
+  const res = await fetch(`https://ping-tool.example.com/ping?host=${host}`)
+  if (!res.ok) {
+    throw new Error("HTTP error:" + res.status);
+  }
+  const data = await res.text();
+  return data;
 });
-
-// async function pingHost(host) {
-//   pingRunning = true;
-//   let pingResultElement = document.querySelector(".new-span-class")
-//   if (!pingResultElement) {
-//     pingResultElement = document.createElement("span");
-//     pingResultElement.classList.add("new-span-class");
-//     pingResultValue.parentNode.appendChild(pingResultElement);
-//   }
-//   while (pingRunning) {
-//     try {
-//       //python疎通確認
-//       pingRunning = true;
-//       const startTime = new Date().getTime(); // ping開始時間
-//       const result = await eel.ping_host(host)();
-//       const endTime = new Date().getTime(); // ping終了時間
-//       const pingTime = endTime - startTime; // ping実行時間
-
-//       pingResultElement.textContent = `${pingTime}ミリ秒 )`;
-//       viewerModeText.innerText = "オンライン "
-//       onlineStatusIndicator.style.backgroundColor = "lightgreen"
-
-//       await new Promise(resolve => setTimeout(resolve, 1000));
-
-//     } catch (error) {
-//       viewerModeText.innerText = "オフライン "
-//       onlineStatusIndicator.style.backgroundColor = "lightgray"
-//     }
-//   }
-// }
-
-
