446457af963768cc184f9aa9d7ecdd6a8265bf89
yuto <y.yatsumimi@gmail.com>
2023-10-04 17:18:15 +0900

    add .gitignore

--- app.py
+++ app.py
@@ -1,72 +1,24 @@
-# --------subprocessを使ったコード--------------##
 import eel
-import subprocess
 import time
-import threading
+from ping3 import ping, EXCEPTIONS
 
 eel.init("web")
 
-ping_runnning = True
-
-# ページが読み込まれた際に、JSのlocalStorageに保存されているIPアドレス(host)に対し、pingを開始する。
 @eel.expose
 def ping_host(host):
-    global ping_runnning
-    while ping_runnning:
+    while True:
         try:
-            res = subprocess.run(
-                ["ping", host, "-c", "1", "-W", "3000"],  # 1秒おきに、pingを実行する。
-                stdout=subprocess.PIPE,
-                text=True,
-            )
-            response = res.stdout
-            print("test: **** ", response)
-
-            if "ping: cannot resolve" in response:
-                # "Unknown host" エラーを明示的に示す
-                response = "Unknown host error"
-
-            # pingを実行して、得られた応答をもとに、UI上の表示を更新する
-            eel.display_update_ping(response)
-
-        except subprocess.CalledProcessError as e:
-            return "Ping failed"
+            result = ping(host)
+            if result is not None:
+                response = f"time={result} ms"
+                print(response)
+                eel.update_ping_result(response)
+            else:
+                eel.update_ping_result("Request timed out.")
+        except EXCEPTIONS.TimeoutException:
+            pass
 
         time.sleep(1)
 
-
-# 古いIPアドレス(host)とのping疎通を終了する。
-@eel.expose
-def kill_ping_process():
-    global ping_runnning
-    ping_runnning = False
-
-def delete_old_ip_address(host):
-    try:
-        # ファイルからIPアドレス一覧を読み込む。rは読み取りモード、読み込み専用で開く。
-        with open("ip_addresses.txt", "r") as file:
-            ip_addresses = file.readlines()
-
-        # 指定されたIPアドレスを削除
-        ip_addresses = [ip.strip() for ip in ip_addresses if ip.strip() != host]
-
-        # 削除後のIPアドレス一覧をファイルに書き込む。wは書き込みモード、ファイルが存在しない場合は新規作成、存在する場合は、中身を上書き
-        with open("ip_addresses.txt", "w") as file:
-            file.writelines("\n".join(ip_addresses))
-
-        return "IP address deleted successfully."
-
-    except Exception as e:
-        return f"An error occurred: {str(e)}"
-
-
-# 新しいIPアドレス（newHost)に対して、pingを開始するために、ping_host関数に新しい引数を渡す。
-@eel.expose
-def restart_ping_host(new_host):
-    global ping_runnning
-    ping_runnning = True
-    threading.Thread(target=ping_host, args=(new_host,)).start()  # スレッドでping_host()を実行
-
-
 if __name__ == "__main__":
-    eel.start("index.html", size=(1024, 768), port=8083)
+    eel.start("index.html", size=(1024,768), port=8083)
--- web/ping.js
+++ web/ping.js
@@ -1,8 +1,4 @@
-//IPアドレス関係
-const ipAddressInput = document.getElementById("ip-address-input")
-const connectIpAddressBtn = document.getElementById("connect-ip-address-btn")
-const cameraViewer = document.getElementById("camera-viewer")
-
+//IPアドレス編集モード⇔読み込みモード切り替え関数のまとめ
 function editIpAddressMode() {
   connectIpAddressBtn.innerText = "保存";
   ipAddressInput.disabled = false;
@@ -17,75 +13,73 @@ function importIpAddressMode() {
   connectIpAddressBtn.style.background = "#D9E5FF";
 }
 
-//ping疎通 pythonとやりとり
-const pingValue = document.getElementById("ping-value")
-const onlineStatusText = document.getElementById("online-status-text")
-const onlineStatusIndicator = document.getElementById("online-status-indicatator")
-
-//ping疎通が、とれている時のUI表示
-function display_update_ping(result) {
-  if (result.includes("Unknown host error")) {
-    displayOfflineEvent();
-  } else {
-    const match = result.match(/time=(\d+\.\d+)\s*ms/);
-    const pingTime = match ? match[1] : "N/A";
-    pingValue.textContent = `（通信時間：${pingTime}ミリ秒）`;
-    onlineStatusText.innerText = "オンライン";
-    onlineStatusIndicator.style.backgroundColor = "lightgreen";
-  }
-};
-eel.expose(display_update_ping);
-
+//オフライン⇔オンラインイベント関数のまとめ
 function displayOfflineEvent() {
-  pingValue.textContent = "(通信データなし)"
-  onlineStatusText.innerText = "オフライン "
+  onOfflineText.innerText = "オフライン "
   onlineStatusIndicator.style.backgroundColor = "lightgray"
+  pingResultValue.textContent = "(通信データなし)";
 };
 
-//①IPアドレスの値が入力される。②保存ボタンを押したら、ping疎通を開始する。
-connectIpAddressBtn.addEventListener('click', () => {
+function displayOnlineEvent() {
+  onOfflineText.innerText = "オンライン";
+  onlineStatusIndicator.style.backgroundColor = "lightgreen";
+}
+
+//IPアドレス関係
+const ipAddressInput = document.getElementById("ip-address-input")
+const connectIpAddressBtn = document.getElementById("connect-ip-address-btn")
+const cameraViewer = document.getElementById("camera-viewer")
+const pingResultValue = document.getElementById("ping-result-value")
+const onlineStatusIndicator = document.getElementById("online-status-indicatator")
+const onOfflineText = document.getElementById("on-offline-text")
+
+eel.expose(update_ping_result);
+function update_ping_result(result) {
+  const match = result.match(/time=(\d+\.\d+)\s*ms/);
+  const pingTime = match[1]
+  pingResultValue.textContent = `（通信時間：${pingTime}ミリ秒）`;
+}
+
+connectIpAddressBtn.addEventListener('click', async () => {
+  const host = ipAddressInput.value;//pingが通信する相手は、入力されたIPアドレス
   try {
-    eel.kill_ping_process(); //古いIPアドレスと疎通切断
-    eel.delete_old_ip_address(host);
-    let newHost = ipAddressInput.value;//newHostに新しいIPアドレスの値を代入
-    eel.restart_ping_host(newHost) //新しいIPアドレスと疎通開始
-    display_update_ping(newHost); //UIに反映
+    const res = eel.ping_host(host);
+    displayOnlineEvent();
   } catch (error) {
-    displayOfflineEvent(); //疎通が取れなかった際に、オフライン表示
-    pingLoopActive = false;
+    displayOfflineEvent();
   }
   if (connectIpAddressBtn.innerText === "編集") {
     editIpAddressMode();
-  } else {
+  } else if (connectIpAddressBtn.innerText === "保存") {
     importIpAddressMode();
   }
+  await eel.restart_ping_host(newHost)();
 });
-
-//疎通がとれているときのpingループの設定
-let pingLoopActive = true;
-
-async function pingLoop(host) {
-  while (pingLoopActive) {
-    try {
-      const response = await eel.ping_host(host)();
-      display_update_ping(response);
-    } catch (error) {
-      displayOfflineEvent();
-      pingLoopActive = false;
-    }
-  }
-}
-
-function importStoredIpAddress() {
-  const storedIpAddress = localStorage.getItem('ip-address');
-  ipAddressInput.value = storedIpAddress;
-  cameraViewer.src = "http://" + storedIpAddress + "/ImageViewer?Mode=Motion&Resolution=640x360&Quality=Standard&Interval=10";
-}
 
 //ページを読み込んだ際の挙動
 window.addEventListener('load', async () => {
   importStoredIpAddress();
   const host = storedIpAddress;//ローカルストレージの値をhostに代入
+  if (storedIpAddress) {
+    ipAddressInput.value = storedIpAddress;
+    cameraViewer.src = "http://" + storedIpAddress + "/ImageViewer?Mode=Motion&Resolution=640x360&Quality=Standard&Interval=10";
+  }
+  let pingLoopActive = true;
 
-  pingLoop(host);
+  async function pingLoop() {
+    while (pingLoopActive) {
+      try {
+        const res = await eel.ping_host(host);
+        if (res === "Ping failed") {
+          displayOfflineEvent();
+        } else {
+          displayOnlineEvent();
+        }
+      } catch (error) {
+        displayOfflineEvent();
+      }
+      await new Promise(relolve => setTimeout(relolve, 1000));
+    }
+  }
+  pingLoop();
 });
